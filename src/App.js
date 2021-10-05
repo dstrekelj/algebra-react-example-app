d7dc9aa68c5bdcc5d13d52a928c6abd5be4e6319
Domagoj Štrekelj <domagojstrekelj@gmail.com>
2021-10-05 19:20:41 +0200

    Add chat context and message list

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import "./App.css";
 import MessageForm from "./components/MessageForm";
 import { useContext, useState } from "react";
-import Message from "./components/Message";
-import { getId } from "./helpers";
 import AppContext from "./contexts/AppContext";
+import ChatContext from "./contexts/ChatContext";
+import MessageList from "./components/MessageList";
 
-const numbers = [1, 2, 3, 4];
-const numberElements = numbers.map(number =>
-  ({ key: getId(), value: number })
-);
+function greet(greeting, name) {
+  console.log(greeting, name);
+}
+greet('Hello', 'Domagoj');
+
+function withSmile(greetFunction) {
+  return function(greeting, name) {
+    greetFunction(greeting, name);
+    console.log(':)');
+  }
+}
+const smileyGreeting = withSmile(greet);
+smileyGreeting('Hello', 'Domagoj');
 
 function App() {
   const appContext = useContext(AppContext);
@@ -19,42 +28,31 @@ function App() {
   }
 
   return (
-    <div className="App">
-      {numberElements.map((numberElement) =>
-        <button key={numberElement.key}>
-          {numberElement.value}
-        </button>
-      )}
-      <header className="App__header">
-        <h1>My Chat App {appContext.language}</h1>
-      </header>
-      <main className="App__main">
-        <div className="App__message-container">
-          {messageObjects.length === 0 && <p>No messages</p>}
-          {messageObjects.map((messageObject, index) =>
-            <Message
-              key={index}
-              isImportant={messageObject.isImportant}
-              message={messageObject.message}
-              title={messageObject.title}
-            />
-          )}
-        </div>
-        <div className="App__message-form">
-          <MessageForm onSendMessage={handleSendMessage} />
-        </div>
-      </main>
-      <footer className="App__footer">
-       <a
-          className="App-link"
-          href="https://github.com/dstrekelj"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          My GitHub Profile
-        </a>
-      </footer>
-    </div>
+    <ChatContext.Provider value={messageObjects}>
+      <div className="App">
+        <header className="App__header">
+          <h1>My Chat App {appContext.language}</h1>
+        </header>
+        <main className="App__main">
+          <div className="App__message-container">
+            <MessageList />
+          </div>
+          <div className="App__message-form">
+            <MessageForm onSendMessage={handleSendMessage} />
+          </div>
+        </main>
+        <footer className="App__footer">
+        <a
+            className="App-link"
+            href="https://github.com/dstrekelj"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            My GitHub Profile
+          </a>
+        </footer>
+      </div>
+    </ChatContext.Provider>
   );
 }
 
