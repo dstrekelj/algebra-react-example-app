fe15ce520b061f5b56e20baa08c1eefa8d606ae7
Domagoj Štrekelj <domagojstrekelj@gmail.com>
2021-10-13 20:40:59 +0200

    Add OrderForm state

--- src/pages/OrderPage/index.js
+++ src/pages/OrderPage/index.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { createStore } from 'redux';
 import { Provider, useDispatch, useSelector } from 'react-redux';
 import reducer, { addOrderItem } from '../../reducers/OrderReducer';
@@ -5,16 +6,22 @@ import reducer, { addOrderItem } from '../../reducers/OrderReducer';
 const orderStore = createStore(reducer);
 
 function OrderForm() {
+  const [state, setState] = useState({ text: '' });
   const dispatch = useDispatch();
+
+  const handleOnChange = (event) => {
+    setState({ text: event.target.value });
+  }
 
   const handleOnSubmit = (event) => {
     event.preventDefault();
-    dispatch(addOrderItem({ text: "Hrana" }));
+    dispatch(addOrderItem(state));
   }
 
   return (
     <form onSubmit={handleOnSubmit}>
-      <input value="Hrana" /><button>Add order</button>
+      <input value={state.text} onChange={handleOnChange} />
+      <button>Add order</button>
     </form>
   );
 }
